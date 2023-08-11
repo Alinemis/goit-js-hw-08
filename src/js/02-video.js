510b62fbdc8262cbd6b466b0d1290751a5b95b57
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-08-11 22:05:20 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_LS = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const seconds = JSON.parse(localStorage.getItem(KEY_LS)) ?? 0;
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(seconds);
+
+function onPlay(data) {
+  localStorage.setItem(KEY_LS, JSON.stringify(data.seconds));
+}
