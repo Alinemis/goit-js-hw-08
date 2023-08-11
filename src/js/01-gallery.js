3c85cb7482c2e6a244debc37c488d743aff8a1c7
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-08-11 18:07:53 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const list = document.querySelector('.gallery');
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href= "${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+list.insertAdjacentHTML('beforeend', createMarkUp(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
